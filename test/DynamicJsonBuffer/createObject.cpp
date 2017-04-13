80698898b8eefe322642a3c96fbafa0aede28a83
Benoit Blanchon <github@benoitblanchon.fr>
2017-04-13 14:10:03 +0200

    Added createObject()

--- test/DynamicJsonBuffer/createObject.cpp
+++ test/DynamicJsonBuffer/createObject.cpp
@@ -6,20 +6,20 @@
 // If you like this project, please add a star!
 
 #include <ArduinoJson.h>
-#include <gtest/gtest.h>
+#include <catch.hpp>
 
-TEST(DynamicJsonBuffer_Object_Tests, GrowsWithObject) {
+TEST_CASE("DynamicJsonBuffer::createObject()") {
   DynamicJsonBuffer json;
 
   JsonObject &obj = json.createObject();
-  ASSERT_EQ(JSON_OBJECT_SIZE(0), json.size());
+  REQUIRE(JSON_OBJECT_SIZE(0) == json.size());
 
   obj["hello"] = 1;
-  ASSERT_EQ(JSON_OBJECT_SIZE(1), json.size());
+  REQUIRE(JSON_OBJECT_SIZE(1) == json.size());
 
   obj["world"] = 2;
-  ASSERT_EQ(JSON_OBJECT_SIZE(2), json.size());
+  REQUIRE(JSON_OBJECT_SIZE(2) == json.size());
 
   obj["world"] = 3;  // <- same key, should not grow
-  ASSERT_EQ(JSON_OBJECT_SIZE(2), json.size());
+  REQUIRE(JSON_OBJECT_SIZE(2) == json.size());
 }
