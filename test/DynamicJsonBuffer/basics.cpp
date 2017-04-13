c2a0205120b16502d612607c8fa82e776dccbc0e
Benoit Blanchon <github@benoitblanchon.fr>
2017-04-13 14:02:50 +0200

    Added basics

--- test/DynamicJsonBuffer/basics.cpp
+++ test/DynamicJsonBuffer/basics.cpp
@@ -6,29 +6,7 @@
 // If you like this project, please add a star!
 
 #include <ArduinoJson.h>
-#include <gtest/gtest.h>
-
-class DynamicJsonBuffer_Basic_Tests : public testing::Test {
- protected:
-  DynamicJsonBuffer buffer;
-};
-
-TEST_F(DynamicJsonBuffer_Basic_Tests, InitialSizeIsZero) {
-  ASSERT_EQ(0, buffer.size());
-}
-
-TEST_F(DynamicJsonBuffer_Basic_Tests, SizeIncreasesAfterAlloc) {
-  buffer.alloc(1);
-  ASSERT_LE(1U, buffer.size());
-  buffer.alloc(1);
-  ASSERT_LE(2U, buffer.size());
-}
-
-TEST_F(DynamicJsonBuffer_Basic_Tests, ReturnDifferentPointer) {
-  void* p1 = buffer.alloc(1);
-  void* p2 = buffer.alloc(2);
-  ASSERT_NE(p1, p2);
-}
+#include <catch.hpp>
 
 static bool isAligned(void* ptr) {
   const size_t mask = sizeof(void*) - 1;
@@ -36,24 +14,45 @@ static bool isAligned(void* ptr) {
   return (addr & mask) == 0;
 }
 
-TEST_F(DynamicJsonBuffer_Basic_Tests, Alignment) {
-  // make room for tow but not three
-  buffer = DynamicJsonBuffer(2 * sizeof(void*) + 1);
+TEST_CASE("DynamicJsonBuffer basics") {
+  DynamicJsonBuffer buffer;
 
-  ASSERT_TRUE(isAligned(buffer.alloc(1)));  // this on is aligned by design
-  ASSERT_TRUE(isAligned(buffer.alloc(1)));  // this one fits in the first block
-  ASSERT_TRUE(isAligned(buffer.alloc(1)));  // this one requires a new block
-}
+  SECTION("InitialSizeIsZero") {
+    REQUIRE(0 == buffer.size());
+  }
 
-TEST_F(DynamicJsonBuffer_Basic_Tests, strdup) {
-  char original[] = "hello";
-  char* copy = buffer.strdup(original);
-  strcpy(original, "world");
-  ASSERT_STREQ("hello", copy);
-}
+  SECTION("SizeIncreasesAfterAlloc") {
+    buffer.alloc(1);
+    REQUIRE(1U <= buffer.size());
+    buffer.alloc(1);
+    REQUIRE(2U <= buffer.size());
+  }
 
-TEST_F(DynamicJsonBuffer_Basic_Tests, strdup_givenNull) {
-  const char* original = NULL;
-  char* copy = buffer.strdup(original);
-  ASSERT_EQ(NULL, copy);
+  SECTION("ReturnDifferentPointer") {
+    void* p1 = buffer.alloc(1);
+    void* p2 = buffer.alloc(2);
+    REQUIRE(p1 != p2);
+  }
+
+  SECTION("Alignment") {
+    // make room for two but not three
+    buffer = DynamicJsonBuffer(2 * sizeof(void*) + 1);
+
+    REQUIRE(isAligned(buffer.alloc(1)));  // this on is aligned by design
+    REQUIRE(isAligned(buffer.alloc(1)));  // this one fits in the first block
+    REQUIRE(isAligned(buffer.alloc(1)));  // this one requires a new block
+  }
+
+  SECTION("strdup") {
+    char original[] = "hello";
+    char* copy = buffer.strdup(original);
+    strcpy(original, "world");
+    REQUIRE(std::string("hello") == copy);
+  }
+
+  SECTION("strdup_givenNull") {
+    const char* original = NULL;
+    char* copy = buffer.strdup(original);
+    REQUIRE(NULL == copy);
+  }
 }
